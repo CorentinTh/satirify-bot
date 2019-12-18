975f15baf9305f3adcd3793d311736d738dc9bf8
CorentinTh <corentin.thomasset74@gmail.com>
2019-12-18 21:28:30 +0100

    refactor: removed useless http server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Twit, { Twitter } from 'twit';
-import {createServer} from 'http';
 
 const BOT_NAME = process.env.BOT_NAME || '@satirify';
 
@@ -13,25 +12,22 @@ const twit = new Twit({
 const satirify = (s: string): string => s.split('').reduce((a: string, l: string, i: number) => a += (i % 2) ? l.toUpperCase() : l.toLowerCase(), '');
 
 // Handle new mention
-twit.stream('statuses/filter', {track: BOT_NAME})
+twit
+    .stream('statuses/filter', { track: BOT_NAME })
     .on('connect', () => console.log(`Connected, listenning to ${BOT_NAME}.`))
     .on('disconnect', () => console.log(`Disconnected.`))
     .on('tweet', async (mention: Twitter.Status) => {
-        try{
-            if(mention.in_reply_to_status_id_str){
-                const original:Twitter.Status = <Twitter.Status>(await twit.get('statuses/show/:id', {id: mention.in_reply_to_status_id_str})).data;
-    
-                if (original.text){
+        try {
+            if (mention.in_reply_to_status_id_str) {
+                const original: Twitter.Status = <Twitter.Status>(await twit.get('statuses/show/:id', { id: mention.in_reply_to_status_id_str })).data;
+
+                if (original.text) {
                     const response = `${satirify(original.text)}\n@${mention.user.screen_name} @${original.user.screen_name}`;
-                    console.log(response);
-                    
+
                     await twit.post('statuses/update', { status: response, in_reply_to_status_id: original.id_str });
                 }
             }
-        }catch(e){
+        } catch (e) {
             console.error(e);
-        }     
+        }
     });
-
-// To check if server is alive
-createServer((req, res) => res.writeHead(200, {'Content-Type': 'text/plain'}).end('ok')).listen(process.env.PORT || 3000);
